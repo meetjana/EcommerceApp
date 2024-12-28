588e55f302651e9e06e37397166790b0e1528e82
meetjana <meetjana.au.hp@gmail.com>
2024-12-28 18:50:10 +0530

    Modified variables.tf and terraform.tfvars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,23 @@
 variable "location" {
   default = "East US"
 }
+# variables.tf
+variable "subscription_id" {
+  description = "The subscription ID for Azure."
+  type        = string
+}
+
+variable "client_id" {
+  description = "The client ID for Azure."
+  type        = string
+}
+
+variable "client_secret" {
+  description = "The client secret for Azure."
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "The tenant ID for Azure."
+  type        = string
+}
