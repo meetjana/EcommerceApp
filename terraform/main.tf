83c8f2e269a51ba67b7107d68fa712decd0ea98c
meetjana <meetjana.au.hp@gmail.com>
2024-12-29 18:19:11 +0530

    Disables and TF workspace and TF version in Main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,24 @@
-terraform {
-  required_version = ">= 1.1.0"
-  requried_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~> 1.5.0"
-    }
-  }
-  cloud {
-    organization = "upskilljana"
-    workspaces {
-      name = "AZWorkspaceTF"
-    }
-  }
-}
+#terraform {
+#  required_version = ">= 1.1.0"
+#  requried_providers {
+#    azurerm = {
+#      source = "hashicorp/azurerm"
+#      version = "~> 1.5.0"
+#    }
+#  }
+#  cloud {
+#    organization = "upskilljana"
+#    workspaces {
+#      name = "AZWorkspaceTF"
+#    }
+#  }
+#}
 provider "azurerm" {
   features {}
 
   subscription_id = var.subscription_id
   client_id       = var.client_id
-#  client_secret   = var.client_secret
+  client_secret   = var.client_secret
   tenant_id       = var.tenant_id
 }
 
